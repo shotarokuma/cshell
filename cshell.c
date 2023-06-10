2ddd3b108f6461e1cfc9c216dd82181339e927d3
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2023-06-09 17:08:53 -0700

    basic cshell

--- cshell.c
+++ cshell.c
@@ -1,28 +1,96 @@
-#include "cshell.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-const int MAX_INPUT = 100;
+#include <unistd.h>
+#include <sys/wait.h>
+#include "cshell.h"
 
-void operate()
+void modifyInput(char command[MAX_ARGS][MAX_INPUT], char input[])
 {
-  // TODO
+  int i = 0;
+  char *token = strtok(input, " ");
+  while (token != NULL)
+  {
+    strcpy(command[i], token);
+    i++;
+    token = strtok(NULL, " ");
+  }
+  strcpy(command[i], "\0");
 }
 
-void interactiveMode(CShell cshell)
+void execute(char command[MAX_ARGS][MAX_INPUT], CShell *cshell)
+{
+  if (strcmp(command[0], "log") == 0)
+  {
+    CShell_printLogs(cshell);
+  }
+  else if (strcmp(command[0], "print") == 0)
+  {
+    CShell_print(cshell, command);
+  }
+  else if (strcmp(command[0], "theme") == 0)
+  {
+    CShell_theme(cshell, command[1]);
+  }
+  else if (command[0][0] == '$')
+  {
+    char name[MAX_INPUT];
+    char value[MAX_INPUT];
+    strcpy(name, strtok(command[0], "="));
+    strcpy(value, strtok(NULL, "="));
+    CShell_appendEnv(cshell, name + 1, value);
+  }
+}
+
+void operate(CShell *cshell, char input[])
+{
+  char command[MAX_ARGS][MAX_INPUT];
+  modifyInput(command, input);
+  pid_t pid = fork();
+
+  if (pid < 0)
+  {
+    exit(1);
+  }
+  else if (pid != 0)
+  {
+    wait(NULL);
+  }
+  else if (pid == 0)
+  {
+    execute(command, cshell);
+    exit(0);
+  }
+}
+
+void interactiveMode(CShell *cshell)
 {
   char input[MAX_INPUT];
-  while(!strcmp(input, "exit"))
+  printf("%s", cshell->name);
+
+  while (fgets(input, sizeof(input), stdin))
   {
-    operate();
+    input[strcspn(input, "\n")] = '\0';
+    if(strcmp(input, "exit") == 0)
+    {
+      break;
+    }
+    operate(cshell, input);
+    CShell_appendLogs(cshell, input);
+
+    printf("%s", cshell->name);
   }
 }
 
-
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-  CShell cshell;
-  if(argc == 1){
+  CShell *cshell = malloc(sizeof(CShell));
+  CShell_init(cshell);
+
+  if (argc == 1)
+  {
     interactiveMode(cshell);
   }
+  CShell_cleanup(cshell);
   return 0;
 }
--- cshell.h
+++ cshell.h
@@ -7,34 +7,39 @@
 
 #define MAX_SIZE 999
 #define LIMIT_SIZE 999
+#define MAX_INPUT 256
+#define MAX_ARGS 10
 
-typedef struct {
-  char* name;
+typedef struct
+{
+  char *name;
   time_t time;
 } Command;
 
-typedef struct {
-  char* name;
-  char* value;
+typedef struct
+{
+  char *name;
+  char *value;
 } EnvVar;
 
-typedef struct {
+typedef struct
+{
   int currLogs;
   int currEnvs;
   Command logs[LIMIT_SIZE];
   EnvVar envs[LIMIT_SIZE];
+  char name[8];
 } CShell;
 
-void CShell_init(CShell* cshell);
-void CShell_appendEnv(CShell* cshell, const char* name, const char* value);
-void CShell_appendLogs(CShell* cshell, const char* name);
-
-void CShell_init(CShell* cshell) {
+void CShell_init(CShell *cshell)
+{
   cshell->currLogs = 0;
   cshell->currEnvs = 0;
+  strcpy(cshell -> name, "cshell$ ");
 }
 
-void CShell_appendEnv(CShell* cshell, const char* name, const char* value) {
+void CShell_appendEnv(CShell *cshell, const char *name, const char *value)
+{
   EnvVar newEnv;
   newEnv.name = strdup(name);
   newEnv.value = strdup(value);
@@ -42,13 +47,93 @@ void CShell_appendEnv(CShell* cshell, const char* name, const char* value) {
   cshell->currEnvs++;
 }
 
-void CShell_appendLogs(CShell* cshell, const char* name) {
+void CShell_appendLogs(CShell *cshell, const char *name)
+{
   time_t t = time(NULL);
   Command newCom;
   newCom.name = strdup(name);
   newCom.time = t;
   cshell->logs[cshell->currLogs] = newCom;
   cshell->currLogs++;
+}
+
+void CShell_printLogs(CShell *cshell)
+{
+  for (int i = 0; i < cshell->currLogs; i++)
+  {
+    Command log = cshell->logs[i];
+    printf("%ld", log.time);
+    printf("%s", log.name);
+  }
+}
+
+void CShell_print(CShell *cshell, char command[MAX_ARGS][MAX_INPUT])
+{
+  char args[MAX_SIZE];
+  strcpy(args, "");
+  int i = 0;
+  while(strcmp(command[i], "\0") != 0)
+  {
+    if(i == 1 && command[1][0] == '$')
+    {
+      for(int j = 0; j < cshell -> currEnvs; j++)
+      {
+        if(strcmp(cshell -> envs[j].name, &command[0][1]) == 0)
+        {
+           printf("%s\n", cshell -> envs[i].value);
+           return;
+        }
+      }
+    }
+    if(i != 0)
+    {
+       strcat(args, command[i]);
+       strcat(args, " ");
+    }
+    i += 1;
+  }
+  printf("%s\n", args);
+}
+
+void CShell_theme(CShell *cShell, char *input)
+{
+  if (strcmp(input, "red") == 0)
+  {
+    printf("\x1B[31m");
+  }
+  else if (strcmp(input, "green") == 0)
+  {
+    printf("\x1B[32m");
+  }
+  else if (strcmp(input, "yellow") == 0)
+  {
+    printf("\x1B[33m");
+  }
+  else if (strcmp(input, "blue") == 0)
+  {
+    printf("\x1B[34m");
+  }
+  else if (strcmp(input, "magent") == 0)
+  {
+    printf("\x1B[35m");
+  }
+  else if (strcmp(input, "cyan") == 0)
+  {
+    printf("\x1B[36m");
+  }
+  else if (strcmp(input, "white") == 0)
+  {
+    printf("\x1B[37m");
+  }
+  else if (strcmp(input, "black") == 0)
+  {
+    printf("\x1B[30m");
+  }
+}
+
+void CShell_cleanup(CShell *cShell)
+{
+  free(cShell);
 }
 
 #endif
